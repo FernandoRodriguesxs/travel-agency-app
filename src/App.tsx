4e100fb4a0844a139dc9a929986bd28fd597ae3b
Fernando Rodrigues <Fernando.hardd@hotmail.com>
2023-09-18 01:23:09 -0300

    animation

--- src/App.tsx
+++ src/App.tsx
@@ -23,6 +23,7 @@ import ResortImg2 from './assets/resort2.jpg'
 import ResortImg3 from './assets/resort3.jpg'
 import HotelImg from './assets/resort-1.jpg'
 import HotelImg2 from './assets/resort-2.jpg'
+import { Slide, Fade } from 'react-awesome-reveal'
 
 export const App = () => {
   return (
@@ -47,152 +48,166 @@ export const App = () => {
         </div>
       </Hero>
       <Wrapper>
-        <AboutSection>
-          <div className="about-left">
-            <h2>
-              About Thailand Outdoor
-              <br />
-              Adventures
-            </h2>
-            <p>
-              Thailand is a Paradise For Adventure Seekers, with A Diverse
-              Landscape That
-              <br />
-              Offers A Range Of Outdoor Activities For Every Level Of Fitness
-              And Interest.
-              <br />
-              From Hiking Through Lush Jungles To Scuba Diving In Crystal-Clear
-              Waters,
-              <br />
-              Thailand Outdoor Adventures
-            </p>
-            <picture>
-              <img src={imgLeft} alt="" />
-            </picture>
-          </div>
-          <div className="about-right">
-            <picture>
-              <img src={imgRight} alt="" />
-            </picture>
-          </div>
-        </AboutSection>
+        <Slide direction="right">
+          <AboutSection>
+            <div className="about-left">
+              <h2>
+                About Thailand Outdoor
+                <br />
+                Adventures
+              </h2>
+              <p>
+                Thailand is a Paradise For Adventure Seekers, with A Diverse
+                Landscape That
+                <br />
+                Offers A Range Of Outdoor Activities For Every Level Of Fitness
+                And Interest.
+                <br />
+                From Hiking Through Lush Jungles To Scuba Diving In
+                Crystal-Clear Waters,
+                <br />
+                Thailand Outdoor Adventures
+              </p>
+              <picture>
+                <img src={imgLeft} alt="" />
+              </picture>
+            </div>
+            <div className="about-right">
+              <picture>
+                <img src={imgRight} alt="" />
+              </picture>
+            </div>
+          </AboutSection>
+        </Slide>
       </Wrapper>
       <Wrapper>
-        <TitleColumn>
-          <h2>Thailand Outdoor Destination</h2>
-        </TitleColumn>
-      </Wrapper>
-      <Wrapper>
-        <CardOutdoor>
-          <div className="card">
-            <picture>
-              <img src={Card1} alt="" />
-            </picture>
-            <h2>CHIANG MAI</h2>
-            <p>
-              Chiang Mai Is A Vibrant City That Offers <br />
-              A Unique Blend Of Ancient Culture And <br />
-              Modern Amenities
-            </p>
-          </div>
-          <div className="card">
-            <picture>
-              <img src={Card2} alt="" />
-            </picture>
-            <h2>BANGKOK</h2>
-            <p>
-              Chiang Mai Is A Vibrant City That Offers <br />
-              A Unique Blend Of Ancient Culture And <br />
-              Modern Amenities
-            </p>
-          </div>
-          <div className="card">
-            <picture>
-              <img src={Card3} alt="" />
-            </picture>
-            <h2>KRABI</h2>
-            <p>
-              Chiang Mai Is A Vibrant City That Offers <br />
-              A Unique Blend Of Ancient Culture And <br />
-              Modern Amenities
-            </p>
-          </div>
-          <div className="card">
-            <picture>
-              <img src={Card4} alt="" />
-            </picture>
-            <h2>PAI</h2>
-            <p>
-              Chiang Mai Is A Vibrant City That Offers <br />
-              A Unique Blend Of Ancient Culture And <br />
-              Modern Amenities
-            </p>
-          </div>
-        </CardOutdoor>
-      </Wrapper>
-      <Wrapper>
-        <ResortText>
+        <Slide direction="left">
           <TitleColumn>
-            <h2>Top 10 Resorts</h2>
+            <h2>Thailand Outdoor Destination</h2>
           </TitleColumn>
-        </ResortText>
+        </Slide>
       </Wrapper>
       <Wrapper>
-        <ResortCards>
-          <picture>
-            <img src={ResortImg1} alt="" />
-          </picture>
-          <picture>
-            <img src={ResortImg2} alt="" />
-          </picture>
-          <picture>
-            <img src={ResortImg3} alt="" />
-          </picture>
-        </ResortCards>
+        <Slide direction="left">
+          <CardOutdoor>
+            <div className="card">
+              <picture>
+                <img src={Card1} alt="" />
+              </picture>
+              <h2>CHIANG MAI</h2>
+              <p>
+                Chiang Mai Is A Vibrant City That Offers <br />
+                A Unique Blend Of Ancient Culture And <br />
+                Modern Amenities.
+              </p>
+            </div>
+            <div className="card">
+              <picture>
+                <img src={Card2} alt="" />
+              </picture>
+              <h2>BANGKOK</h2>
+              <p>
+                Bangkok Is A Bustling Metropolis That <br />
+                Combines Ancient Traditions With <br />
+                Modern Innovation.
+              </p>
+            </div>
+            <div className="card">
+              <picture>
+                <img src={Card3} alt="" />
+              </picture>
+              <h2>KRABI</h2>
+              <p>
+                Krabi Is Known Its Stunning <br />
+                Limestone Cliffs, Crystal-Clear Waters, <br />
+                And Secluded Beaches.
+              </p>
+            </div>
+            <div className="card">
+              <picture>
+                <img src={Card4} alt="" />
+              </picture>
+              <h2>PAI</h2>
+              <p>
+                Thailand, Pai Is A Haven For Travelers <br />
+                Seeking A Laid-Back And Peaceful <br />
+                Atmosphere
+              </p>
+            </div>
+          </CardOutdoor>
+        </Slide>
       </Wrapper>
       <Wrapper>
-        <TitleColumn>
-          <h2>Our Latest Blog</h2>
-        </TitleColumn>
+        <Slide direction="left">
+          <ResortText>
+            <TitleColumn>
+              <h2>Top 10 Resorts</h2>
+            </TitleColumn>
+          </ResortText>
+        </Slide>
       </Wrapper>
       <Wrapper>
-        <HotelCards>
-          <div className="cards-hotel">
+        <Slide direction="left">
+          <ResortCards>
             <picture>
-              <img src={HotelImg} alt="" />
+              <img src={ResortImg1} alt="" />
             </picture>
-            <h3>Discovering The Best Street Food In Bangkok</h3>
-            <p>
-              This Blog Post Could Explore The Rich And Diverse Street Food
-              Scene In The Capital
-              <br />
-              City Of Thailand
-            </p>
-            <a href="#">Read More</a>
-          </div>
-          <div className="cards-hotel">
             <picture>
-              <img src={HotelImg2} alt="" />
+              <img src={ResortImg2} alt="" />
             </picture>
-            <h3>Discovering The Best Street Food In Bangkok</h3>
-            <p>
-              This Blog Post Could Explore The Rich And Diverse Street Food
-              Scene In The Capital
-              <br />
-              City Of Thailand
-            </p>
-            <a href="#">Read More</a>
-          </div>
-        </HotelCards>
+            <picture>
+              <img src={ResortImg3} alt="" />
+            </picture>
+          </ResortCards>
+        </Slide>
+      </Wrapper>
+      <Wrapper>
+        <Slide direction="left">
+          <TitleColumn>
+            <h2>Our Latest Blog</h2>
+          </TitleColumn>
+        </Slide>
+      </Wrapper>
+      <Wrapper>
+        <Slide direction="left">
+          <HotelCards>
+            <div className="cards-hotel">
+              <picture>
+                <img src={HotelImg} alt="" />
+              </picture>
+              <h3>Discovering The Best Street Food In Bangkok</h3>
+              <p>
+                This Blog Post Could Explore The Rich And Diverse Street Food
+                Scene In The Capital City Of Thailand
+              </p>
+              <a href="#">Read More</a>
+            </div>
+            <div className="cards-hotel">
+              <picture>
+                <img src={HotelImg2} alt="" />
+              </picture>
+              <h3>Exploring The Hidden Gems Of Chiang Mai</h3>
+              <p>
+                This Blog Post Could Highlight Some Of The Lesser-Know Equally
+                Stunning
+                <br />
+                Attractions In The Northern City Of Chiang Mai
+              </p>
+              <a href="#">Read More</a>
+            </div>
+          </HotelCards>
+        </Slide>
       </Wrapper>
       <SubscribeSection>
-        <div className="text-sub">
-          <h3>Subscribe Today To Get Latest Updates !</h3>
-        </div>
-        <div className="newsletter">
-          <input type="text" placeholder="Email" />
-          <button>SUBSCRIBE</button>
-        </div>
+        <Slide direction="up">
+          <div className="text-sub">
+            <h3>Subscribe Today To Get Latest Updates !</h3>
+          </div>
+          <div className="newsletter">
+            <input type="text" placeholder="Email" />
+            <button>SUBSCRIBE</button>
+          </div>
+        </Slide>
       </SubscribeSection>
     </>
   )
