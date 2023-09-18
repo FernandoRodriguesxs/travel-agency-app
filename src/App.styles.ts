99b4e977fb243229cea0183acc6b12ff888b3c8b
Fernando Rodrigues <Fernando.hardd@hotmail.com>
2023-09-18 14:36:19 -0300

    error responsive

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -7,10 +7,6 @@ export const Hero = styled.div`
 
   height: 100vh;
 
-  @media screen and (max-width: 768px) {
-    height: auto;
-  }
-
   background:
     linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)),
     url('../src/assets/hero-img.jpg') no-repeat;
